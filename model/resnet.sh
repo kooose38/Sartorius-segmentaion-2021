e43af08f69e43dd7ec708fc112bc792ddcdadde4
kooose38 <75005025+kooose38@users.noreply.github.com>
2021-10-30 20:37:05 +0900

    [DELETE MODEL]

--- model/resnet.sh
+++
@@ -1,8 +0,0 @@
-#!bin/sh
-
-!pip install /kaggle/input/segmentation-models-wheels/efficientnet_pytorch-0.6.3-py3-none-any.whl
-!pip install /kaggle/input/segmentation-models-wheels/pretrainedmodels-0.7.4-py3-none-any.whl
-!pip install /kaggle/input/segmentation-models-wheels/timm-0.3.2-py3-none-any.whl
-!pip install /kaggle/input/segmentation-models-wheels/segmentation_models_pytorch-0.1.3-py3-none-any.whl
-!mkdir -p /root/.cache/torch/hub/checkpoints/
-!cp ../input/pytorch-pretrained-image-models/resnet34.pth /root/.cache/torch/hub/checkpoints/resnet34-333f7ec4.pth
--- model/resnet34.py
+++
@@ -1,7 +0,0 @@
-import collections.abc as container_abcs
-torch._six.container_abcs = container_abcs
-import segmentation_models_pytorch as smp 
-
-def build_model(model_name="resnet34"):
-   model = smp.Unet(model_name, encoder_weights="imagenet", activation=None)
-   return model 
